ad59bd5ecee394f7c4d1f8a7b627c3e1b9865189
Bruno Abrantes <inf0rmer.realm@gmail.com>
2012-07-14 11:26:15 +0100

    code cleanup

--- jquery.responsiveimages.js
+++ jquery.responsiveimages.js
@@ -1,31 +1,31 @@
 (function( $ ){
 	jQuery.fn.responsiveImages = function( options ) {
-		
+
 		var settings = {
 			'sizes' 			: [480, 768, 992, 1382],
 			'resizeEvent' 		: true,
 			'bustCache' 		: false
 		};
-		
+
 		if ( options ) {
 			$.extend( settings, options );
 		}
-		
+
 		return this.each(function(){
 			var $this = this,
 				handler,
 				lastSize,
 				sizes = settings.sizes;
-		
+
 			handler = function() {
 				// Calculate screen size
 				var screenWidth = document.documentElement.clientWidth,
 					sizeToUse;
-					
+
 				if (lastSize == screenWidth) {
 					return;
 				}
-				
+
 				// Determine in which bracket the screen width falls into
 				for (var i = sizes.length; i--; ) {
 					if (screenWidth >= sizes[i]) {
@@ -33,20 +33,23 @@
 						break;
 					}
 				}
-				
+
 				lastSize = sizeToUse = (!sizeToUse) ? 480 : sizeToUse;
-				
+
 				// Loop through all elements and change the source
-				var $imgs = $($this).find('img');
-				
+				var $imgs = $this.add($this.find('img'));
+
 				$imgs.each(function(){
 					var $img = $(this);
-					$img.attr('src', $img.attr('data-src-' + sizeToUse));
+					$img
+					.attr('src', $img.attr('data-src-' + sizeToUse))
+					.removeAttr('width')
+					.removeAttr('height');
 				});
 			};
-			
+
 			handler.apply();
-			
+
 			// Declare a window resize event to call the handler function
 			if (settings.resizeEvent) {
 				$(window).resize(handler);
